c326257a874a85b42b5849543ebc6988d2cfe20f
invokerLm <lium@novasoftware.cn>
2017-09-14 23:45:32 +0800

    登陆界面初始化

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 $import("./js/pages/login.js");
 
-;(function($,win,mus){
-    win.appSetting={
-        contentContainer:$("#main-content"),
-        pageSource:{
-            login:"./temPages/loginPage.html",
-            taskPage:"./temPages/taskPage.html",
+;(function ($, win, mus) {
+    win.appSetting = {
+        pageSource: {
+            login: "./temPages/loginPage.html",
+            taskPage: "./temPages/taskPage.html",
         }
     };
-    win.initMain=function(){
-        $.get(this.appSetting.pageSource.login,function (template) {
-        var renderPage=mus.render(template);
-        this.contentContainer.html(renderPage);
+    win.initMain = function () {
+        var mainContainer = $("#main-content");
+        var bindEvents = function () {
+            $("#btn-login").click(function () {
+                alert("click login btn");
+            })
+        }
+        $.get(this.appSetting.pageSource.login, function (template) {
+            var renderPage = mus.render(template);
+            mainContainer.html(renderPage);
+            bindEvents();
         });
+
+
     }
-    win.initMain();
-})(jQuery,window,Mustache);
+})(jQuery, window, Mustache);
+
+$(document).ready(function () {
+    window.initMain();
+});
